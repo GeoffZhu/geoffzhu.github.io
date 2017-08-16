c8dc8b448beaf9458ad37cdc5efd56c50b74e7ff
Wis <geoffzhu1992@gmail.com>
2017-08-16 11:22:37 +0800

    Update index.js

--- error-report/index.js
+++ error-report/index.js
@@ -1,7 +1,7 @@
 window.onerror = function(msg, url, line, col, error) {
   // 直接将错误打印到控制台
   console.log(arguments)
-
+  console.log(window.event)
   // 方便在未打开控制台的时候，记录错误对象
   window.demoError = arguments
 
